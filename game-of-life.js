4137338b3e9fa2a4985590442a473b94c42f1166
github username <juliannne@gmail.com>
2018-05-12 12:09:33 -0400

    Add drawCanvas function

--- game-of-life.js
+++ game-of-life.js
@@ -5,6 +5,7 @@ let rows = myCanvas.height / pixel;
 let ctx = myCanvas.getContext("2d");
 let grid;
 
+// make empty 2D array
 function makeGrid(cols, rows) {
   let arr = new Array();
   for (let i = 0; i < cols; i++) {
@@ -13,11 +14,26 @@ function makeGrid(cols, rows) {
   return arr;
 }
 
+// fill cells randomly with 0 and 1
 function fillRandom() {
   grid = makeGrid(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = Math.floor(Math.random() * 2);
+    }
+  }
+}
+
+function drawCanvas() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j] == 1) {
+        ctx.fillStyle = "#33DDFF";
+        ctx.fillRect((i * pixel), (j * pixel), pixel - 1, pixel - 1);
+      } else {
+        ctx.fillStyle = "#F1F4F4";
+        ctx.fillRect((i * pixel), (j * pixel), pixel - 1, pixel - 1);
+      }
     }
   }
 }
